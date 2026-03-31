a140eab9750d0a28f8036d43fbdebb53c68bba0d
user <user@example.com>
2026-03-31 19:25:42 +0800

    chore: setup .gitignore and remove build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_learpc_learpc_app_LeaRPcastApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.learpc.learpc.app.LeaRPcastApplication"
-)
-public final class _com_learpc_learpc_app_LeaRPcastApplication {
-}
